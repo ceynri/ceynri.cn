0c4d7199af1780ea4e913300436cca90e3fb8033
ceynri <347670115@qq.com>
2020-01-15 02:02:12 +0800

    添加复制内容至剪贴板功能；添加新页面打开链接

--- js/homepage.js
+++ js/homepage.js
@@ -1,7 +1,24 @@
 'use strict';
 
+// 将字符串复制到剪贴板
+let _copyToCilpboard = (str) => {
+    const strWrapper = document.createElement('input');
+    strWrapper.setAttribute('readonly', 'readonly');
+    strWrapper.setAttribute('value', str);
+    document.body.appendChild(strWrapper);
+    strWrapper.select();
+    if (document.execCommand('copy')) {
+        if (document.execCommand('copy')) {
+            console.log('复制内容：' + str);
+        } else {
+            console.error('复制失败');
+        }
+    }
+    document.body.removeChild(strWrapper);
+};
+
 // icons
-(function () {
+(() => {
 
     let intro = document.getElementById('intro');
     let githubIntro = document.getElementById('githubIntro');
@@ -13,61 +30,80 @@
     let wechatIcon = document.getElementsByClassName('icon-wechat')[0];
     let qqIcon = document.getElementsByClassName('icon-qq')[0];
     let mailIcon = document.getElementsByClassName('icon-mail')[0];
-    
+
     let iconWrappers = document.getElementsByClassName('icon-wrapper');
 
     for (let i = 0; i < iconWrappers.length; i++) {
         let wrapper = iconWrappers[i];
-        wrapper.children[0].addEventListener('mouseover', function () {
+        wrapper.children[0].addEventListener('mouseover', () => {
             // icon
-            this.style.opacity = 0.0;
+            wrapper.children[0].style.opacity = 0.0;
             wrapper.children[1].style.opacity = 1.0;
             // text
             intro.style.opacity = 0.0;
+            info.style.opacity = 1.0;
         });
 
-        wrapper.children[0].addEventListener('mouseout', function () {
+        wrapper.children[0].addEventListener('mouseout', () => {
             // icon
-            this.style.opacity = 1.0;
+            wrapper.children[0].style.opacity = 1.0;
             wrapper.children[1].style.opacity = 0.0;
             // text
             intro.style.opacity = 1.0;
-            info.innerHTML = '';
+            info.style.opacity = 0.0;
         });
     }
-    
+
     let info = document.getElementById('introInfo');
-    let wechatInfo = '点击按钮复制微信号';
-    let qqInfo = '点击按钮复制QQ号';
 
     // over
-    githubIcon.addEventListener('mouseover', function () {
+    githubIcon.addEventListener('mouseover', () => {
         githubIntro.style.opacity = 1.0;
+        info.innerHTML = 'GitHub';
     })
-    wechatIcon.addEventListener('mouseover', function () {
+    wechatIcon.addEventListener('mouseover', () => {
         wechatIntro.style.opacity = 1.0;
-        info.innerHTML = wechatInfo;
+        info.innerHTML = 'WeChat 点击复制';
     })
-    qqIcon.addEventListener('mouseover', function () {
+    qqIcon.addEventListener('mouseover', () => {
         qqIntro.style.opacity = 1.0;
-        info.innerHTML = qqInfo;
+        info.innerHTML = 'QQ 点击复制';
     })
-    mailIcon.addEventListener('mouseover', function () {
+    mailIcon.addEventListener('mouseover', () => {
         mailIntro.style.opacity = 1.0;
+        info.innerHTML = 'Mail';
     })
     // out
-    githubIcon.addEventListener('mouseout', function () {
+    githubIcon.addEventListener('mouseout', () => {
         githubIntro.style.opacity = 0.0;
     })
-    wechatIcon.addEventListener('mouseout', function () {
+    wechatIcon.addEventListener('mouseout', () => {
         wechatIntro.style.opacity = 0.0;
     })
-    qqIcon.addEventListener('mouseout', function () {
+    qqIcon.addEventListener('mouseout', () => {
         qqIntro.style.opacity = 0.0;
     })
-    mailIcon.addEventListener('mouseout', function () {
+    mailIcon.addEventListener('mouseout', () => {
         mailIntro.style.opacity = 0.0;
     })
-    
+    // click
+    wechatIcon.addEventListener('click', () => {
+        _copyToCilpboard('WeChat: sakuramemory');
+    })
+    qqIcon.addEventListener('click', () => {
+        _copyToCilpboard('QQ: 347670115');
+    })
+    mailIcon.addEventListener('click', () => {
+        _copyToCilpboard('ceynri@gmail.com');
+    })
 
+})();
+
+// 新页面从新标签页打开
+(() => {
+    let socIcons = document.getElementById('socialIcons');
+    let aTags = socIcons.getElementsByTagName('a');
+    for (let i = 0; i < aTags.length; i++) {
+        aTags[i].target = '_blank';
+    }
 })();
