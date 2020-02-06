a3477e3e9e7b756b82f6b3f1fc79b6b3b4ecf83d
ceynri <347670115@qq.com>
2020-02-06 22:42:32 +0800

    完善代码，为装饰线条添加视差

--- js/parallax.js
+++ js/parallax.js
@@ -1,45 +1,66 @@
-    class Parallax {
-        constructor() {
-            this.items = [];
-            // this.easing = Power1.easeOut;
-            // this.EASE_SPEED = .75;
-        }
-        addParallax(elem, ratio) {
-            // 检查输入是否有值，避免误操作
-            if (elem && ratio) {
+const TOP = 0;
+const CENTER = 1;
+const BOTTOM = 2;
+
+class Parallax {
+    constructor() {
+        this.items = [];
+    }
+    addParallax(elem, parallaxRate, positionRatio) {
+        // 检查输入是否有值，避免误操作
+        if (elem && parallaxRate) {
+            if (!Array.isArray(elem)) {
                 this.items.push({
-                    elem: elem,
-                    ratio: ratio,
-                });
-            }
-            return this;
-        }
-        renderScrollParallax() {
-            // * 渲染滚动页面
-            const frame = () => {
-                this.items.forEach((item) => {
-                    // 使item滚动
-                    TweenLite.set(item.elem, {
-                        y: this.getScrollTop(item.elem) * (item.ratio - 1),
-                        // ease: this.easing
+                    elem,
+                    parallaxRate,
+                    positionRatio
+                }); // key和value名称相同的情况下可以简写为一个
+            } else {
+                elem.forEach(elem => {
+                    this.items.push({
+                        elem,
+                        parallaxRate,
+                        positionRatio
                     });
                 });
-                // 循环下去
-                requestAnimationFrame(frame);
             }
-            requestAnimationFrame(frame);
         }
-        getPageScroll() {
-            return window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
-        }
-        getScrollTop(elem) {
-            return elem.getBoundingClientRect().top - document.documentElement.clientHeight;
-        }
+        return this;
     }
 
-    const aboutHeader = document.querySelector('.about-header');
-    const aboutText = document.querySelector('.about-text');
-    const parallax = new Parallax();
-    parallax.addParallax(aboutHeader, 1.2)
-        .addParallax(aboutText, .5);
-    parallax.renderScrollParallax();
+    renderScrollParallax() {
+        // * 渲染滚动页面
+        const frame = () => {
+            this.items.forEach((item) => {
+                // 使item滚动
+                TweenLite.set(item.elem, {
+                    y: this.getScroll(item.elem, item.positionRatio) * (item.parallaxRate - 1),
+                });
+            });
+            // 循环下去
+            requestAnimationFrame(frame);
+        }
+        requestAnimationFrame(frame);
+    }
+    getPageScroll() {
+        return window.pageYOffset || document.body.scrollTop || document.documentElement.scrollTop;
+    }
+    getScroll(elem, rate) {
+        return elem.getBoundingClientRect().top - rate * (document.documentElement.clientHeight / 2);
+    }
+}
+
+const aboutHeader = document.querySelector('.about-header');
+const aboutText = document.querySelector('.about-text');
+
+const lines = [];
+lines.push(document.querySelector('.hero .title h1 span:nth-child(1) *:last-child'));
+lines.push(document.querySelector('.hero .title h1 span:nth-child(2) *:last-child'));
+lines.push(document.querySelector('.hero .title h2 *:last-child'));
+lines.push(document.querySelector('.date-line'));
+
+const parallax = new Parallax();
+parallax.addParallax(aboutHeader, 1.2, BOTTOM)
+    .addParallax(aboutText, .5, BOTTOM)
+    .addParallax(lines, 1.2, CENTER);
+parallax.renderScrollParallax();
