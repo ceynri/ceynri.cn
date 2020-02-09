538219874b79d5af83759250ddd72624dcfc1d13
ceynri <347670115@qq.com>
2020-02-09 14:01:54 +0800

    添加works部分的手机响应式界面样式

--- js/ceynriUtils.js
+++ js/ceynriUtils.js
@@ -16,13 +16,21 @@ const CeynriUtils = {
 
 const MediaMatcher = {
     isTouchScreenDevice: () => {
+        // < ipad pro 12.9
         const portraitLimit = window.matchMedia("screen and (max-device-width: 1024px) and (orientation: portrait)").matches;
         const landscapeLimit = window.matchMedia("screen and (max-device-width: 1366px) and (orientation: landscape)").matches;
         return portraitLimit || landscapeLimit;
     },
     isTabletDevice: () => {
+        // ipad (mini) ~ ipad pro 12.9
         const portraitLimit = window.matchMedia("screen and (min-device-width: 768px) and (max-device-width: 1024px) and (orientation: portrait)").matches;
         const landscapeLimit = window.matchMedia("screen and (min-device-width: 1024px) and (max-device-width: 1366px) and (orientation: landscape)").matches;
         return portraitLimit || landscapeLimit;
     },
+    isMobileDevice: () => {
+        // iphone4 ~ ipad
+        const portraitLimit = window.matchMedia("screen and (min-device-width: 320px) and (max-device-width: 767px) and (orientation: portrait)").matches;
+        const landscapeLimit = window.matchMedia("screen and (min-device-width: 480px) and (max-device-width: 1023px) and (orientation: landscape)").matches;
+        return portraitLimit || landscapeLimit;
+    }
 }
