4051c33669d50c77ab8866ba65a97e11effec9fc
ceynri <347670115@qq.com>
2020-02-09 20:58:15 +0800

    重构contact.js，改为面向对象写法，使用TweenLite做渐变动画

--- js/contact.js
+++ js/contact.js
@@ -1,107 +1,134 @@
-/* 
- * 图标与鼠标交互效果
- */
 {
-    function copyToCilpboard(str) {
-        // * 将字符串复制到剪贴板
-        const strWrapper = document.createElement('input');
-        strWrapper.setAttribute('readonly', 'readonly');
-        strWrapper.setAttribute('value', str);
-        document.body.appendChild(strWrapper);
-        strWrapper.select();
-        if (document.execCommand('copy')) {
-            if (document.execCommand('copy')) {
-                console.log('复制内容：' + str);
-            } else {
-                console.error('复制失败');
-            }
+    class Contact {
+        constructor(text, defaultText, iconBtn, info, infoText) {
+            this.initElem(text, defaultText, iconBtn, info, infoText);
+            this.initConst();
+            this.initEvent();
         }
-        document.body.removeChild(strWrapper);
-    };
-
-    const defaultInfo = document.querySelector('.info-default');
-    const iconInfo = document.querySelector('.icon-info');
-    const iconInfoColor = getComputedStyle(iconInfo, null).getPropertyValue('color');
-    const iconInfoClickedColor = '#ffc83d';
-
-    const iconWrappers = document.querySelectorAll('.icon-wrapper');
-    const infoContainer = document.querySelector('.info-container');
-
-    iconWrappers.forEach((wrapper, i) => {
-        const icon = wrapper.children[0];
-        wrapper.addEventListener('mouseover', () => {
-            // icon
-            icon.children[0].style.opacity = 0.0;
-            icon.children[1].style.opacity = 1.0;
-            // text
-            defaultInfo.style.opacity = 0.0;
-            iconInfo.style.opacity = 1.0;
-            // section
-            infoContainer.children[i + 1].style.opacity = 1.0;
-            infoContainer.children[i + 1].style.visibility = 'inherit';
-        });
-
-        wrapper.addEventListener('mousedown', () => {
-            // 按下时icon变透明
-            icon.children[1].style.opacity = 0.25;
-        });
-        wrapper.addEventListener('click', () => {
+        initElem(text, defaultText, iconBtn, info, infoText) {
+            this.text = text;
+            this.defaultText = defaultText;
+            this.iconBtn = iconBtn;
+            this.icons = iconBtn.querySelector('.icons');
+            this.info = info;
+            this.infoText = infoText;
+        }
+        initConst() {
+            this.ICON_INFO_COLOR = getComputedStyle(document.documentElement, null).getPropertyValue('--textColor');
+            this.ICON_INFO_CLICK_COLOR = '#ffc83d';
+            this.OPACITY = .8;
+            this.FADE_SPEED = .3;
+        }
+        initEvent() {
+            this.listenMouseEvent();
+        }
+        listenMouseEvent() {
+            const mouseOverAnimation = () => {
+                TweenLite.to(this.icons.children[0], this.FADE_SPEED, {
+                    opacity: 0,
+                    ease: Power3.ease
+                });
+                TweenLite.to(this.icons.children[1], this.FADE_SPEED, {
+                    opacity: 1,
+                    ease: Power3.ease
+                });
+                TweenLite.to(this.defaultText, this.FADE_SPEED, {
+                    opacity: 0,
+                    ease: Power3.easeOut
+                });
+                TweenLite.to(this.text, this.FADE_SPEED, {
+                    opacity: 1,
+                    ease: Power3.ease
+                });
+                TweenLite.to(this.info, this.FADE_SPEED, {
+                    opacity: 1,
+                    ease: Power3.ease
+                });
+                this.info.innerHTML = this.infoText;
+            };
             // iconInfo文字变色特效
-            iconInfo.style.color = iconInfoClickedColor;
-        });
-        wrapper.addEventListener('mouseup', () => {
-            icon.children[1].style.opacity = 1.0;
-        });
+            const infoHighLightTween = TweenLite.to(this.info, this.FADE_SPEED, {
+                color: this.ICON_INFO_CLICK_COLOR,
+                paused: true
+            });
+            const mouseOutAnimation = () => {
+                TweenLite.to(this.icons.children[0], this.FADE_SPEED, {
+                    opacity: 1,
+                    ease: Power3.ease
+                });
+                TweenLite.to(this.icons.children[1], this.FADE_SPEED, {
+                    opacity: 0,
+                    ease: Power3.ease
+                });
+                TweenLite.to(this.defaultText, this.FADE_SPEED, {
+                    opacity: 1,
+                    ease: Power3.easeIn
+                });
+                TweenLite.to(this.text, this.FADE_SPEED, {
+                    opacity: 0,
+                    ease: Power3.ease
+                });
+                TweenLite.to(this.info, this.FADE_SPEED, {
+                    opacity: 0,
+                    ease: Power3.ease
+                });
+                infoHighLightTween.reverse();
+            };
 
-        wrapper.addEventListener('mouseout', () => {
-            // icon
-            icon.children[0].style.opacity = 1.0;
-            icon.children[1].style.opacity = 0.0;
-            // text
-            defaultInfo.style.opacity = 1.0;
-            iconInfo.style.opacity = 0.0;
-            iconInfo.style.color = iconInfoColor;
-            // section
-            infoContainer.children[i + 1].style.opacity = 0.0;
-            infoContainer.children[i + 1].style.visibility = 'hidden';
-        });
-    });
+            this.iconBtn.addEventListener('mouseover', mouseOverAnimation);
+            this.iconBtn.addEventListener('click', () => {
+                infoHighLightTween.play();
+            });
+            this.iconBtn.addEventListener('mouseout', mouseOutAnimation);
+        }
+        addClickCopyString(str, infoText = '已复制😊') {
+            this.iconBtn.addEventListener('click', () => {
+                this.copyToClipboard(str);
+                this.info.innerHTML = infoText;
+            });
+        }
+        copyToClipboard(str) {
+            // * 将字符串复制到剪贴板
+            const strWrapper = document.createElement('input');
+            strWrapper.setAttribute('readonly', 'readonly');
+            strWrapper.setAttribute('value', str);
+            document.body.appendChild(strWrapper);
+            strWrapper.select();
+            if (document.execCommand('copy')) {
+                if (document.execCommand('copy')) {
+                    console.log('复制内容：' + str);
+                } else {
+                    console.error('复制失败');
+                }
+            }
+            document.body.removeChild(strWrapper);
+        };
+    }
 
-    const githubIcon = document.querySelector('.icon-github');
-    const wechatIcon = document.querySelector('.icon-wechat');
-    const qqIcon = document.querySelector('.icon-qq');
-    const bilibiliIcon = document.querySelector('.icon-bilibili');
-    const mailIcon = document.querySelector('.icon-mail');
+    const contact = document.querySelector('.contact');
 
-    // over
-    // TODO 改为大字背景
-    githubIcon.addEventListener('mouseover', () => {
-        iconInfo.innerHTML = 'GitHub';
-    })
-    wechatIcon.addEventListener('mouseover', () => {
-        iconInfo.innerHTML = 'WeChat';
-    })
-    qqIcon.addEventListener('mouseover', () => {
-        iconInfo.innerHTML = 'QQ';
-    })
-    bilibiliIcon.addEventListener('mouseover', () => {
-        iconInfo.innerHTML = 'bilibili';
-    })
-    mailIcon.addEventListener('mouseover', () => {
-        iconInfo.innerHTML = 'Mail';
-    })
+    const defaultText = contact.querySelector('.contact-default');
+    const githubText = contact.querySelector('.contact-github');
+    const bilibiliText = contact.querySelector('.contact-bilibili');
+    const mailText = contact.querySelector('.contact-mail');
+    const wechatText = contact.querySelector('.contact-wechat');
+    const qqText = contact.querySelector('.contact-qq');
 
-    // click
-    wechatIcon.addEventListener('click', () => {
-        copyToCilpboard('sakuramemory');
-        iconInfo.innerHTML = '已复制微信号😊';
-    })
-    qqIcon.addEventListener('click', () => {
-        copyToCilpboard('347670115');
-        iconInfo.innerHTML = '已复制QQ号😊';
-    })
-    mailIcon.addEventListener('click', () => {
-        copyToCilpboard('ceynri@gmail.com');
-        iconInfo.innerHTML = '已复制邮箱😊';
-    })
+    const githubBtn = contact.querySelector('.github-btn');
+    const bilibiliBtn = contact.querySelector('.bilibili-btn');
+    const mailBtn = contact.querySelector('.mail-btn');
+    const wechatBtn = contact.querySelector('.wechat-btn');
+    const qqBtn = contact.querySelector('.qq-btn');
+
+    const btnInfo = contact.querySelector('.btn-info');
+
+    const github = new Contact(githubText, defaultText, githubBtn, btnInfo, 'Github');
+    const bilibili = new Contact(bilibiliText, defaultText, bilibiliBtn, btnInfo, 'bilibili');
+    const mail = new Contact(mailText, defaultText, mailBtn, btnInfo, 'Mail');
+    const wechat = new Contact(wechatText, defaultText, wechatBtn, btnInfo, 'WeChat');
+    const qq = new Contact(qqText, defaultText, qqBtn, btnInfo, 'QQ');
+
+    wechat.addClickCopyString('sakuramemory', '已复制微信号😊');
+    qq.addClickCopyString('347670115', '已复制QQ号😊');
+    mail.addClickCopyString('ceynri@gmail.com', '已复制邮箱😊');
 }
