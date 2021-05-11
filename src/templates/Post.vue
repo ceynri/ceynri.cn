64de8e622cb13861936b561d4b4c6cebd942daa2
ceynri <ceynri@gmail.com>
2021-05-11 15:57:47 +0800

    feat: Adjust style; extract the transition duration as css var

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -14,7 +14,7 @@
         <g-image alt="Cover image" v-if="$page.post.cover_image" :src="$page.post.cover_image" />
       </div>
 
-      <div class="post__content" v-html="$page.post.content" />
+      <div class="post__content link-highlight" v-html="$page.post.content" />
 
       <div class="post__footer">
         <PostTags :post="$page.post" />
