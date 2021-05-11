f71444ca791b337dde8ede5377cde4655f5bf151
ceynri <ceynri@gmail.com>
2021-05-11 16:01:09 +0800

    feat: import svg as html text; add social icon

---
+++ src/config.js
@@ -0,0 +1,24 @@
+const icons = require('~/assets/images/icons');
+
+module.exports = {
+  social: {
+    github: {
+      url: 'https://github.com/ceynri',
+      tooltip: '@ceynri',
+      icon: icons.github,
+      color: '#161b22',
+      darkmodeColor: '#fff',
+    },
+    bangumi: {
+      url: 'http://bgm.tv/user/558747',
+      tooltip: 'bangumi',
+      icon: icons.bangumi,
+      color: '#f93292',
+      darkmodeColor: '#f93292',
+    },
+  },
+  beian: {
+    url: 'https://beian.miit.gov.cn/',
+    text: '粤ICP备20009331号',
+  },
+};
