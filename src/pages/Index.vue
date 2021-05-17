7a4011ea3d74509c0e72b7e56de4163c4040461e
ceynri <ceynri@gmail.com>
2021-05-17 21:10:36 +0800

    feat: move blog to /blog/ route

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,59 +1,26 @@
 <template>
-  <Layout :show-logo="false">
-    <section class="hero">
-      <h1 class="hero__title">Ceynri's Blog</h1>
-    </section>
-
-    <!-- List posts -->
-    <div class="posts">
-      <PostCard v-for="edge in $page.posts.edges" :key="edge.node.id" :post="edge.node" />
-    </div>
-  </Layout>
+  <div id="app">
+    <div>Landing page is Work-In-Progress...</div>
+    <g-link to="/blog/">Go to Blog -></g-link>
+  </div>
 </template>
 
-<page-query>
-query {
-  posts: allPost(filter: { published: { eq: true }}) {
-    edges {
-      node {
-        id
-        title
-        date (format: "YYYY.MM.DD")
-        description
-        cover_image (width: 860, height: 400, blur: 10)
-        path
-        tags {
-          id
-          title
-          path
-        }
-      }
-    }
-  }
-}
-</page-query>
-
 <script>
-import PostCard from '~/components/PostCard.vue';
-
 export default {
-  components: {
-    PostCard,
-  },
-  metaInfo: {
-    title: 'Blogs',
-  },
-};
+
+}
 </script>
 
-<style lang="scss">
-.hero {
-  padding: calc(var(--space) / 2) 0;
+<style lang="scss" scoped>
+#app {
+  min-width: 100%;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 
-  &__title {
-    font-size: 3em;
-    color: var(--title-color);
-    text-align: center;
-  }
+  font-size: 3em;
+  text-align: center;
 }
 </style>
