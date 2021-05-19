8ccdc3268fb8d411cc2c2c63d57e3a49c06f353b
ceynri <ceynri@gmail.com>
2021-05-19 21:05:15 +0800

    feat(float.js): implement parallax effect class by gsap.js

---
+++ src/utils/float.js
@@ -0,0 +1,56 @@
+import { TweenLite, Power1 } from 'gsap';
+
+/**
+ * Make element floatable
+ */
+export class Float {
+  constructor() {
+    this.initProp();
+    this.initEvent();
+  }
+  initProp() {
+    this.clientWidth = document.documentElement.clientWidth;
+    this.clientHeight = document.documentElement.clientHeight;
+    this.SPEED = 1;
+  }
+  initEvent() {
+    window.addEventListener('resize', () => {
+      this.clientWidth = document.documentElement.clientWidth;
+      this.clientHeight = document.documentElement.clientHeight;
+    });
+
+    // 监听鼠标移动
+    document.addEventListener(
+      'mousemove',
+      (e) => {
+        this.clientX = e.clientX;
+        this.clientY = e.clientY;
+      },
+      {
+        passive: true,
+      }
+    );
+  }
+  addFloat(elems, level) {
+    if (!Array.isArray(elems)) {
+      this.float(elems, level);
+    } else {
+      elems.forEach((elem) => {
+        this.float(elem, level);
+      });
+    }
+    // 连缀语法
+    return this;
+  }
+  float(elem, level) {
+    const frame = () => {
+      TweenLite.to(elem, this.SPEED, {
+        x: (this.clientWidth / 2 - this.clientX) * level * 0.01,
+        y: (this.clientHeight / 2 - this.clientY) * level * 0.01,
+        ease: Power1.easeOut,
+      });
+      requestAnimationFrame(frame);
+    };
+    requestAnimationFrame(frame);
+  }
+}
