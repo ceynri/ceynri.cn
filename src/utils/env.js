c07e56dcc6045e2db4067926e326fcd9707e72ee
ceynri <ceynri@gmail.com>
2021-05-20 22:58:16 +0800

    refactor: modify to function type for exec at call

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,26 +1,75 @@
-export const ua = navigator.userAgent || '';
+export const getUa = () => {
+  return navigator.userAgent || '';
+};
+
 // 系统
-export const isIPhone = /iPhone|iPod/i.test(ua);
-export const isIPad = /iPad/i.test(ua);
-export const isIos = isIPhone || isIPad;
-export const isAndroid = /Android/i.test(ua);
-export const isOtherPhone = /Windows Phone|IEMobile|SymbianOS/i.test(ua);
+export const isIPhone = () => {
+  const ua = getUa();
+  return /iPhone|iPod/i.test(ua);
+};
+
+export const isIPad = () => {
+  const ua = getUa();
+  return /iPad/i.test(ua);
+};
+
+export const isIos = () => {
+  return isIPhone() || isIPad();
+};
+
+export const isAndroid = () => {
+  const ua = getUa();
+  return /Android/i.test(ua);
+};
+
+export const isOtherPhone = () => {
+  const ua = getUa();
+  return /Windows Phone|IEMobile|SymbianOS/i.test(ua);
+};
+
 // 浏览器
-export const isFireFox = /Firefox/i.test(ua);
-export const isChrome = /Chrome|CriOS/i.test(ua);
-export const isWeChat = /micromessenger/i.test(ua);
+export const isFireFox = () => {
+  const ua = getUa();
+  return /Firefox/i.test(ua);
+};
+
+export const isChrome = () => {
+  const ua = getUa();
+  return /Chrome|CriOS/i.test(ua);
+};
+
+export const isWeChat = () => {
+  const ua = getUa();
+  return /micromessenger/i.test(ua);
+};
+
 // 设备类型
-export const isPad =
-  isIPad ||
-  (isAndroid && !/Mobile/i.test(ua)) ||
-  (isFireFox && /Tablet/i.test(ua)) ||
-  /PlayBook/i.test(ua);
-export const isPhone = (isIPhone || isAndroid || isOtherPhone) && !isPad;
-export const isMobile = isIPhone || isAndroid || isOtherPhone || isPad;
-export const isPc = !isMobile;
+export const isPad = () => {
+  const ua = getUa();
+  return (
+    isIPad() ||
+    (isAndroid() && !/Mobile/i.test(ua)) ||
+    (isFireFox() && /Tablet/i.test(ua)) ||
+    /PlayBook/i.test(ua)
+  );
+};
 
-export const widthMoreThan = (width) =>
-  window.matchMedia(`screen and (min-width: ${width}px)`).matches;
+export const isPhone = () => {
+  return (isIPhone() || isAndroid() || isOtherPhone()) && !isPad();
+};
 
-export const widthLessThan = (width) =>
-  window.matchMedia(`screen and (max-width: ${width}px)`).matches;
+export const isMobile = () => {
+  return isIPhone() || isAndroid() || isOtherPhone() || isPad();
+};
+
+export const isPc = () => {
+  return !isMobile();
+};
+
+export const widthMoreThan = (width) => {
+  return window.matchMedia(`screen and (min-width: ${width}px)`).matches;
+};
+
+export const widthLessThan = (width) => {
+  return window.matchMedia(`screen and (max-width: ${width}px)`).matches;
+};
