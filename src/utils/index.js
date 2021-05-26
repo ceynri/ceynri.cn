93f124ef737041c7687f4059ef93b203ffc09086
ceynri <ceynri@gmail.com>
2021-05-26 18:46:36 +0800

    feat(utils): add throttle and debounce function

---
+++ src/utils/index.js
@@ -0,0 +1,34 @@
+export * as env from './env';
+export { Float } from './float';
+export { default as objFilter } from './objFilter';
+
+function throttle(method, delay = 100, context = this) {
+  let timer = null;
+  return function() {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      timer = null;
+      method.apply(context, arguments);
+    }, delay);
+  };
+}
+
+// 函数去抖
+function debounce(method, delay = 100, context = this) {
+  let timer = null;
+  const start = new Date();
+  return function() {
+    const current = new Date();
+    clearTimeout(timer);
+    if (current - start < delay) {
+      return;
+    }
+    timer = setTimeout(() => {
+      method.apply(context, arguments);
+    }, delay);
+  };
+}
+
+export { throttle, debounce };
