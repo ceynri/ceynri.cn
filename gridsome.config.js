d099c7b743989e8c62c8f0c463730528dc3899dd
ceynri <ceynri@gmail.com>
2021-06-02 11:25:30 +0800

    feat(config): enable Google Analytics #14

--- gridsome.config.js
+++ gridsome.config.js
@@ -19,7 +19,6 @@ module.exports = {
       externalLinksTarget: '_blank',
       externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
       anchorClassName: 'icon icon-link',
-      plugins: ['@gridsome/remark-prismjs'],
     },
   },
 };
