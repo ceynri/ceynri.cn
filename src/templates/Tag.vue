417bb7d4ec2b9ff77e9931311eb31f4d63f3d088
ceynri <ceynri@gmail.com>
2021-06-04 15:34:57 +0800

    feat(Tag): new tag header style #5

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,11 +1,20 @@
 <template>
   <Layout>
-    <h1 class="tag-title text-center space-bottom">
-      # {{ $page.tag.title }}
-    </h1>
+    <header class="tag__header">
+      <h1 class="tag__title space-bottom">
+        <span class="hash">#</span> {{ $page.tag.title.toUpperCase() }}
+      </h1>
+      <div class="tag__info">
+        {{ $page.tag.belongsTo.edges.length }} POST{{ $page.tag.belongsTo.edges.length > 1 ? 'S' : '' }}
+      </div>
+    </header>
 
     <div class="posts">
-      <PostCard v-for="edge in $page.tag.belongsTo.edges" :key="edge.node.id" :post="edge.node"/>
+      <PostCard
+        v-for="edge in $page.tag.belongsTo.edges"
+        :key="edge.node.id"
+        :post="edge.node"
+      />
     </div>
   </Layout>
 </template>
@@ -32,19 +41,52 @@ query Tag ($id: ID!) {
 </page-query>
 
 <script>
-import PostCard from '~/components/PostCard.vue'
+import PostCard from '~/components/PostCard.vue';
 
 export default {
   components: {
-    PostCard
+    PostCard,
   },
-  metaInfo: {
-    title: 'Tags'
-  }
-}
+  metaInfo() {
+    return {
+      title: `# ${this.$page.tag.title}`,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
+.tag {
+  &__header {
+    padding: var(--padding-width);
+    background-color: var(--bg-content-color);
+    transition: background-color var(--duration);
+    border-radius: var(--radius);
+    margin-bottom: calc(var(--padding-width) / 2);
 
+    position: relative;
+    overflow: hidden;
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 10px;
+      height: 100%;
+      background-color: var(--border-color);
+      transition: background-color var(--duration);
+    }
+  }
+
+  &__title {
+    margin: 0;
+  }
+
+  &__info {
+    margin-top: 1em;
+    font-size: 0.8em;
+  }
+}
 </style>
-
