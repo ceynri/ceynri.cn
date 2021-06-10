cb777021e01f77b8a73e4312342d06e41a811a50
ceynri <ceynri@gmail.com>
2021-06-10 17:19:06 +0800

    feat: use vue-svg-inline-loader, remove NavItem (close #16)

--- gridsome.config.js
+++ gridsome.config.js
@@ -21,4 +21,13 @@ module.exports = {
       },
     },
   },
+  chainWebpack: (config) => {
+    config.module
+      .rule('vue')
+      .use('vue-svg-inline-loader')
+      .loader('vue-svg-inline-loader')
+      .options({
+        /* ... */
+      });
+  },
 };
