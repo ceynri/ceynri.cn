f761c094753f98a7bcb3762e0f35780bb9b60f6b
ceynri <ceynri@gmail.com>
2021-06-10 20:21:41 +0800

    chore: remove vuex

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import '~/assets/styles/index.scss';
 import DefaultLayout from '~/layouts/Default.vue';
 import PostLayout from '~/layouts/Post.vue';
-import Vuex from 'vuex';
-import store from '~/store';
 
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
-export default function(Vue, { appOptions, head }) {
+export default function(Vue, { head }) {
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
   Vue.component('PostLayout', PostLayout);
-  Vue.use(Vuex);
-  appOptions.store = store;
   head.htmlAttrs = {
     lang: 'zh_CN',
   };
