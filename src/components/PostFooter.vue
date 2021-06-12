0133d7969e5d8c224c03055c53fa4bfec095783f
ceynri <ceynri@gmail.com>
2021-06-12 23:38:22 +0800

    feat(PostFooter): new footer for last modified #20

---
+++ src/components/PostFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <footer class="post__footer">
+    <blockquote v-if="post.lastmod" class="footer-info">
+      <p>Last modified on {{ post.lastmod }}.</p>
+    </blockquote>
+    <PostTags :tags="post.tags" />
+  </footer>
+</template>
+
+<script>
+import PostTags from '~/components/PostTags';
+
+export default {
+  props: {
+    post: Object,
+  },
+  components: {
+    PostTags,
+  },
+};
+</script>
+
+<style lang="scss">
+.post__footer {
+  margin-top: var(--padding-width);
+
+  .footer-info {
+    color: var(--light-color);
+    font-size: 0.9em;
+  }
+}
+</style>
