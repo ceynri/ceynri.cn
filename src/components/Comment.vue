7bef0afc2529f74298ea0272bf7d1832452d7e18
ceynri <ceynri@gmail.com>
2021-06-21 19:44:53 +0800

    feat(Comment): add comment #17

---
+++ src/components/Comment.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  props: {
+    /**
+     * enum: [0, 1]
+     */
+    enableReactions: {
+      type: Number,
+      default: 1,
+    },
+    /**
+     * enum: ['dark', 'light', 'preferred_color_scheme']
+     */
+    theme: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      scheme: this.theme || this.$store.state.scheme,
+    };
+  },
+  render(h) {
+    return (
+      <div class="giscus">
+        <script
+          src="https://giscus.app/client.js"
+          data-repo="ceynri/giscus-test"
+          data-repo-id="MDEwOlJlcG9zaXRvcnkzNzg4MDIwNTg="
+          data-category="Announcements"
+          data-category-id="MDE4OkRpc2N1c3Npb25DYXRlZ29yeTMzMDQzMjE5"
+          data-mapping="title"
+          data-reactions-enabled={this.enableReactions}
+          data-theme={this.scheme}
+          crossorigin="anonymous"
+          async
+        />
+      </div>
+    );
+  },
+};
+</script>
