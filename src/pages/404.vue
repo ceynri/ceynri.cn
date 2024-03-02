a03d196e84fa68e25ed366ed676a3c4a900905c1
ceynri <ceynri@gmail.com>
2024-03-02 17:17:38 +0800

    feat(404): make words different in different theme

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,16 +1,21 @@
 <template>
   <Layout>
     <div class="content-box">
-      <h1>404🥱</h1>
-      Have a good night.
+      <h1>404{{ scheme === 'dark' ? '🥱' : '🧭' }}</h1>
+      {{ scheme === 'dark' ? 'Have a good night.' : 'Have a nice day!' }}
     </div>
   </Layout>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
   metaInfo: {
     title: '404',
   },
+  computed: {
+    ...mapState(['scheme']),
+  }
 };
 </script>
