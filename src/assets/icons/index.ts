5ca6246b7b60ddb3e09658c306b6c2575ee89475
ceynri <ceynri@gmail.com>
2025-05-21 22:10:15 +0800

    chore: delete unused files

--- src/assets/icons/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as IconArchive } from './archive.svg';
-export { default as IconFeather } from './feather.svg';
-export { default as IconHome } from './home.svg';
-export { default as IconInfo } from './info.svg';
-export { default as IconMoon } from './moon.svg';
-export { default as IconRSS } from './rss.svg';
-export { default as IconSun } from './sun.svg';
